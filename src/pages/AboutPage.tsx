df28037e6d32b394230538fcbe026299eb386078
Przemysław Koziński <pkozinski@future-processing.com>
2026-04-16 21:49:02 +0200

    api/generate: test 9

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,3 +1,17 @@
 export const AboutPage = () => {
-  return <>About page</>;
+  const test = async () => {
+    await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: "cat astronaut" }),
+    });
+  };
+
+  return (
+    <>
+      About page <button onClick={test}>test /api/generate</button>
+    </>
+  );
 };
