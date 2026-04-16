a18e62b095e5ca32f95b28d5a3e805e904975faf
Przemysław Koziński <pkozinski@future-processing.com>
2026-04-16 21:31:34 +0200

    api/generate: test 4

--- api/generate.js
+++ api/generate.js
@@ -1,14 +1,30 @@
+// export default async function handler(req, res) {
+//   const response = await fetch("https://api.openai.com/v1/images", {
+//     method: "POST",
+//     headers: {
+//       Authorization: `Bearer ${process.env.VITE_OPENAI_KEY}`,
+//       "Content-Type": "application/json",
+//     },
+//     body: { prompt: "cat astronaut" },
+//   });
+
+//   const data = await response.json(response);
+//   console.log("!", data);
+//   res.status(200).json(response);
+// }
+
+import OpenAI from "openai";
+
 export default async function handler(req, res) {
-  const response = await fetch("https://api.openai.com/v1/images", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.VITE_OPENAI_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: { prompt: "cat astronaut" },
+  const client = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_KEY,
+    dangerouslyAllowBrowser: true,
   });
 
-  const data = await response.json(response);
-  console.log("!", data);
-  res.status(200).json(response);
+  const result = await client.images.generate({
+    // model: "gpt-image-1",
+    prompt: prompt,
+    size: "1024x1024",
+  });
+  return result?.data ? result.data[0].url : "";
 }
