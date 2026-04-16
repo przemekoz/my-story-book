8d8a96a14451477838171688b46ccd5d80c83e49
Przemysław Koziński <pkozinski@future-processing.com>
2026-04-16 21:25:40 +0200

    api/generate: test 1

---
+++ api/generate.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://api.openai.com/v1/images", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.VITE_OPENAI_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: { prompt: "cat astronaut" },
+  });
+
+  const data = await JSON.parse(response);
+  res.status(200).json(data);
+}
