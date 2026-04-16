a399861fdb6fd29e3a52e029c916bfd00a788901
Przemysław Koziński <pkozinski@future-processing.com>
2026-04-16 21:28:48 +0200

    api/generate: test 3

--- api/generate.js
+++ api/generate.js
@@ -9,5 +9,6 @@ export default async function handler(req, res) {
   });
 
   const data = await response.json(response);
-  res.status(200).json(data);
+  console.log("!", data);
+  res.status(200).json(response);
 }
