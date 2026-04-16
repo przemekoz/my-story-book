b1ad15aac9734830f84601e7477b6986c5159987
Przemysław Koziński <pkozinski@future-processing.com>
2026-04-16 21:21:42 +0200

    converted from js -> ts

--- api/generate.ts
+++ api/generate.ts
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   const response = {
     message: "OK",
-    env: process.env,
+    env: process.env.VITE_OPENAI_KEY,
   };
 
   //   const data = await JSON.parse(response);
