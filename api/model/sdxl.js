8d21dfec21a341d6d400dfc8de2405c759503e10
Przemek Kozinski <pkozinski@future-processing.com>
2026-05-01 13:08:48 +0200

    sdxl test 9

--- api/model/sdxl.js
+++ api/model/sdxl.js
@@ -37,9 +37,32 @@ module.exports = async function handler(req, res) {
     busboy.on("finish", async () => {
       if (prompt) {
         const base64 = `data:${mimeType};base64,${fileBuffer.toString("base64")}`;
-
-        console.log("image", base64);
         console.log("prompt", prompt);
+
+        const response = await fetch(
+          "https://api.replicate.com/v1/predictions",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              version:
+                "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
+              input: {
+                prompt,
+                image: base64,
+                strength: 0.7,
+                width: 512,
+                height: 512,
+              },
+            }),
+          },
+        );
+
+        console.log("response", response);
+
         return res.status(200).json({
           image: `ok`,
         });
