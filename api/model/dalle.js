716b797a7cc4e257ffa43bf4a8927891d316946c
Przemek Kozinski <pkozinski@future-processing.com>
2026-05-01 11:39:22 +0200

    dalle test 1

--- api/model/dalle.js
+++ api/model/dalle.js
@@ -2,30 +2,52 @@ const OpenAI = require("openai");
 
 module.exports = async function handler(req, res) {
   try {
+
     if (req.method !== "POST") {
-      return res.status(405).json({ error: "Method not allowed" });
+      return res.status(405).end()
     }
-    const client = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
+    
+    const busboy = Busboy({ headers: req.headers })
 
-    const { prompt } = JSON.parse(req.body);
+    let prompt = ""
 
-    const result = await client.images.generate({
-      model: "gpt-image-1",
-      prompt,
-      size: "1024x1024",
-    });
+    // handle text fields
+    busboy.on("field", (name, value) => {
+      if (name === "prompt") {
+        prompt = value
+      }
+    })
 
-    const imageBase64 = result.data[0].b64_json;
+    // when done
+    busboy.on("finish", () => {
+      console.log("Prompt:", prompt)
 
-    return res.status(200).json({
-      image: `data:image/png;base64,${imageBase64}`,
-    });
+
+      if (prompt) {
+        const client = new OpenAI({
+          apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        const result = await client.images.generate({
+          model: "gpt-image-1",
+          prompt,
+          size: "1024x1024",
+        });
+
+        const imageBase64 = result.data[0].b64_json;
+
+        return res.status(200).json({
+          image: `data:image/png;base64,${imageBase64}`,
+        });
+      }
+    })
+
+    req.pipe(busboy)
+
   } catch (error) {
     console.error(error);
     return res.status(500).json({
       error: "Dalle Image generation failed",
     });
   }
-};
+}
