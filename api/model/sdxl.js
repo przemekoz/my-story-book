0967127e16e415c0e77dc17d35577a8a8de3edf3
Przemek Kozinski <pkozinski@future-processing.com>
2026-05-01 12:42:29 +0200

    sdxl test 5

--- api/model/sdxl.js
+++ api/model/sdxl.js
@@ -35,29 +35,29 @@ module.exports = async function handler(req, res) {
 
       console.log("image", base64);
 
-      const response = await fetch("https://api.replicate.com/v1/predictions", {
-        method: "POST",
-        headers: {
-          Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          version:
-            "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
-          input: {
-            prompt,
-            image: base64,
-            strength: 0.7,
-            width: 512,
-            height: 512,
-          },
-        }),
-      });
+      // const response = await fetch("https://api.replicate.com/v1/predictions", {
+      //   method: "POST",
+      //   headers: {
+      //     Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     version:
+      //       "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
+      //     input: {
+      //       prompt,
+      //       image: base64,
+      //       strength: 0.7,
+      //       width: 512,
+      //       height: 512,
+      //     },
+      //   }),
+      // });
 
-      console.log("response", response);
-      const prediction = await response.json();
+      // console.log("response", response);
+      // const prediction = await response.json();
 
-      console.log("prediction", prediction);
+      // console.log("prediction", prediction);
 
       // polling (simple version)
       // let result = prediction;
