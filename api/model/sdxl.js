d2dd08c8a885c00f13c1cb2ef51e15b4713673eb
Przemek Kozinski <pkozinski@future-processing.com>
2026-05-01 13:05:42 +0200

    sdxl test 8

--- api/model/sdxl.js
+++ api/model/sdxl.js
@@ -9,6 +9,22 @@ module.exports = async function handler(req, res) {
     const busboy = Busboy({ headers: req.headers });
 
     let prompt = "";
+    let fileBuffer = null;
+    let mimeType = "";
+
+    const chunks = [];
+
+    busboy.on("file", (fieldname, file, info) => {
+      mimeType = info.mimeType;
+
+      file.on("data", (data) => {
+        chunks.push(data);
+      });
+
+      file.on("end", () => {
+        fileBuffer = Buffer.concat(chunks);
+      });
+    });
 
     // handle text fields
     busboy.on("field", (name, value) => {
@@ -19,9 +35,11 @@ module.exports = async function handler(req, res) {
 
     // when done
     busboy.on("finish", async () => {
-      console.log("Prompt:", prompt);
-
       if (prompt) {
+        const base64 = `data:${mimeType};base64,${fileBuffer.toString("base64")}`;
+
+        console.log("image", base64);
+        console.log("prompt", prompt);
         return res.status(200).json({
           image: `ok`,
         });
